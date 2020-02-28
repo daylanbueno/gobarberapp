e890559ad4c1faf33fcac24b98dd9f922fa2bb3a
dailan <dasylansantos@gmail.com>
2020-02-27 22:52:50 -0300

    validação com yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            nome: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res
+                .status(400)
+                .json({ error: 'Validação dos campos falhou' });
+        }
+
         const userExists = await User.findOne({
             where: { email: req.body.email },
         });
@@ -15,9 +32,26 @@ class UserController {
     }
 
     async update(req, res) {
-        const { oldPassword, email } = req.body;
+        const schema = Yup.object().shape({
+            nome: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string(),
+            password: Yup.string().when('oldPassword', (oldPassword, field) =>
+                oldPassword ? field.required() : field
+            ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res
+                .status(400)
+                .json({ error: 'A validação dos campos falhou' });
+        }
 
         const user = await User.findByPk(req.userId);
+        const { oldPassword, email } = req.body;
 
         if (email !== user.email) {
             const userExists = await User.findOne({
