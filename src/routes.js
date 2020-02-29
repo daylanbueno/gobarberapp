016642d97d54f4f4d6a61708ff95f96f78937924
Dailan Bueno <daylansantos@gmail.com>
2020-02-29 10:48:08 -0300

    Ajuste criar sessao nao pedir token

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-routes.post('/sessions', SessionController.store);
 
 routes.get('/', async (req, res) => {
     res.json('Hellow World');
