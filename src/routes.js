35c4445b6e255b0c795f3574c1aa2c2386965bab
Dailan Bueno <daylansantos@gmail.com>
2020-03-15 20:52:40 -0300

    Ajuste para recuperar agendas pessoa fisica

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,5 +23,6 @@ routes.get('/providers', ProviderController.index);
 routes.post('/appointment', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index)
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedules',ScheduleController.index);
 
 export default routes;
