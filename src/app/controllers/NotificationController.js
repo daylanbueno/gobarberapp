8714bac0733d519cd179bcb21d3d9b9d40d63a66
Dailan Bueno <daylansantos@gmail.com>
2020-03-17 21:47:47 -0300

    Listando notificações

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,20 @@
+import User from '../models/User' 
+import Notification from '../../schemas/Notification'
+
+class NotificationController {
+    async index(req, res) {
+        const checkIsProvider = await User.findOne({
+            where: { id: req.userId, provider: true }
+        }) 
+        
+        if (!checkIsProvider) {
+            return res.json({ error: 'User not is provider' })
+        }
+        const notifications = await Notification.find({
+            user: req.userId,
+        }).sort({ createdAt: 'desc' }) // ordena por esse campo
+        .limit(20) // limitar em 20 registro
+        return res.json(notifications)
+    }
+}
+export default new NotificationController() 
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,5 +25,6 @@ routes.post('/appointment', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index)
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/schedules',ScheduleController.index);
+routes.get('/notifications', NotificationController.index)
 
 export default routes;
