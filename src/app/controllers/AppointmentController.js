e935303711199c63bb3aef3272d4f749780097b1
Dailan Bueno <daylansantos@gmail.com>
2020-03-31 21:35:25 -0300

    Criando job para envio  de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../../schemas/Notification';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 const validateSchema = yup.object().shape({
     provider_id: yup.number().required(),
@@ -130,21 +131,9 @@ class AppointmentController {
         }
         appointment.canceled_at = new Date();
         await appointment.save();
-        await Mail.sendMail({
-            to: `${appointment.provider.nome} <${appointment.provider.email}> `,
-            subject: 'Agendamento cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.nome,
-                user: appointment.user.nome,
-                date: format(
-                    appointment.date,
-                    "'dia' dd  'de' MMMM, 'às' H:mm'h' ",
-                    {
-                        locale: pt,
-                    }
-                ),
-            },
+
+        await Queue.add(CancellationMail.key, {
+            appointment,
         });
 
         return res.json(appointment);
---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,30 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+    get key() {
+        return 'CancellationMail';
+    }
+
+    async handle({ data }) {
+        const { appointment } = data;
+        await Mail.sendMail({
+            to: `${appointment.provider.nome} <${appointment.provider.email}> `,
+            subject: 'Agendamento cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.nome,
+                user: appointment.user.nome,
+                date: format(
+                    parseISO(appointment.date),
+                    "'dia' dd  'de' MMMM, 'às' H:mm'h' ",
+                    {
+                        locale: pt,
+                    }
+                ),
+            },
+        });
+    }
+}
+export default new CancellationMail();
