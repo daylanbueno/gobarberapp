53956a1eaa9ceb345ff1f0608bb8b01a6ca068a4
Dailan Bueno <daylansantos@gmail.com>
2020-05-27 21:35:13 -0300

    Estilizando dashboard

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
-import api from '../../services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-export default function Dashboard() {
-    api.get('appointments');
-    return <h1>Dashboard</h1>;
+import { Container, Time } from './styles';
+
+function Dashboard() {
+    return (
+        <Container>
+            <header>
+                <button type="button">
+                    <MdChevronLeft size={36} color="#fff" />
+                </button>
+                <strong>26 de maio </strong>
+                <button type="button">
+                    <MdChevronRight size={36} color="#fff" />
+                </button>
+            </header>
+            <ul>
+                <Time past>
+                    <strong>08:00</strong>
+                    <span>Amanda Cristina</span>
+                </Time>
+                <Time available>
+                    <strong>09:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong>10:00</strong>
+                    <span>Eduardo Silva</span>
+                </Time>
+                <Time available>
+                    <strong>11:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+            </ul>
+        </Container>
+    );
 }
+
+export default Dashboard;
---
+++ web/src/pages/Dashboard/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    max-width: 600px;
+    margin: 50px auto;
+
+    display: flex;
+    flex-direction: column;
+
+    header {
+        display: flex;
+        align-self: center;
+        align-items: center;
+
+        button {
+            border: 0;
+            border-radius: 50%;
+        }
+
+        strong {
+            color: #fff;
+            font-size: 24px;
+            margin: 0 15px;
+        }
+    }
+
+    ul {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 15px;
+        margin-top: 30px;
+    }
+`;
+
+export const Time = styled.div`
+    background: #fff;
+    padding: 20px;
+    border-radius: 4px;
+    opacity: ${(props) => (props.past ? 0.5 : 1)};
+
+    strong {
+        display: block;
+        color: ${(props) => (props.available ? '#33FF3C' : '#FF3352')};
+        font-size: 24px;
+        font-weight: normal;
+    }
+
+    span {
+        display: block;
+        margin-top: 3px;
+        color: #666;
+    }
+`;
